369b30ed532a66b6bb41ec4dbefaef11b33fb259
ing <inyoman_gurnitha@yahoo.com>
2019-11-16 15:41:51 +0700

    Added Footer and Copyright

--- index.php
+++ index.php
@@ -48,7 +48,8 @@
          <!-- col-md-6 Ends -->
       </div>
       <!-- container Ends -->
-   </div><!-- top ends -->
+   </div>
+   <!-- top ends -->
    <!--======= NAVBAR ======= -->
    <div class="navbar navbar-default" id="navbar">
       <!-- navbar navbar-default Starts -->
@@ -141,7 +142,8 @@
          <!-- navbar-collapse collapse Ends -->
       </div>
       <!-- container Ends -->
-   </div><!-- navbar ends -->
+   </div>
+   <!-- navbar ends -->
    <!--======= SLIDER ======= -->
    <div class="container" id="slider">
       <!-- container Starts -->
@@ -189,7 +191,8 @@
          <!-- carousel slide Ends --->
       </div>
       <!-- col-md-12 Ends -->
-   </div><!-- slider ends -->
+   </div>
+   <!-- slider ends -->
    <!--======= ADVANTAGES ======= -->
    <div id="advantages">
       <!-- advantages Starts -->
@@ -246,7 +249,8 @@
          <!-- same-height-row Ends -->
       </div>
       <!-- container Ends -->
-   </div><!-- advantages ends -->
+   </div>
+   <!-- advantages ends -->
    <!--======= HOT ======= -->
    <div id="hot">
       <!-- hot Starts -->
@@ -263,7 +267,8 @@
          <!-- container Ends -->
       </div>
       <!-- box Ends -->
-   </div><!-- hot Ends -->
+   </div>
+   <!-- hot ends -->
    <!--======= MAIN CONTENT ======= -->
    <div id="content" class="container">
       <!-- container Starts -->
@@ -467,9 +472,138 @@
          </div>
       </div>
       <!-- row Ends -->
-   </div><!-- main content nds -->  
-       
-   
+   </div>
+   <!-- main content ends -->  
+   <!--======= FOOTER ======= -->
+   <div id="footer">
+      <!-- footer Starts -->
+      <div class="container">
+         <!-- container Starts -->
+         <div class="row" >
+            <!-- row Starts -->
+            <div class="col-md-3 col-sm-6" >
+               <!-- col-md-3 col-sm-6 Starts -->
+               <h4>Pages</h4>
+               <ul>
+                  <!-- ul Starts -->
+                  <li><a href="cart.php">Shopping Cart</a></li>
+                  <li><a href="contact.php">Contact Us</a></li>
+                  <li><a href="shop.php">Shop</a></li>
+                  <li>
+                     <a href='checkout.php' >My Account</a>
+                  </li>
+               </ul>
+               <!-- ul Ends -->
+               <hr>
+               <h4>User Section</h4>
+               <ul>
+                  <!-- ul Starts -->
+                  <li>
+                     <a href='checkout.php' >Login</a>
+                  </li>
+                  <li><a href="customer_register.php">Register</a></li>
+                  <li><a href="terms.php">Terms And Conditions </a></li>
+               </ul>
+               <!-- ul Ends -->
+               <hr class="hidden-md hidden-lg hidden-sm" >
+            </div>
+            <!-- col-md-3 col-sm-6 Ends -->
+            <div class="col-md-3 col-sm-6">
+               <!-- col-md-3 col-sm-6 Starts -->
+               <h4> Top Products Categories </h4>
+               <ul>
+                  <!-- ul Starts -->
+                  <li> <a href='shop.php?p_cat=4'> Coats </a> </li>
+                  <li> <a href='shop.php?p_cat=5'> T-Shirts </a> </li>
+                  <li> <a href='shop.php?p_cat=6'> Sweater </a> </li>
+                  <li> <a href='shop.php?p_cat=7'> jackets </a> </li>
+                  <li> <a href='shop.php?p_cat=8'> Microsoft Windows </a> </li>
+               </ul>
+               <!-- ul Ends -->
+               <hr class="hidden-md hidden-lg">
+            </div>
+            <!-- col-md-3 col-sm-6 Ends -->
+            <div class="col-md-3 col-sm-6">
+               <!-- col-md-3 col-sm-6 Starts -->
+               <h4>Where to find us</h4>
+               <p>
+                  <!-- p Starts -->
+                  <strong>Computerfever Ltd.</strong>
+                  <br>Saeed Park
+                  <br>Lahore
+                  <br>0923334566931
+                  <br>sad.ahmed22224@gmail.com
+                  <br>
+                  <strong>Mohammed Tahir Ahmed</strong>
+               </p>
+               <!-- p Ends -->
+               <a href="contact.php">Go to Contact us page</a>
+               <hr class="hidden-md hidden-lg">
+            </div>
+            <!-- col-md-3 col-sm-6 Ends -->
+            <div class="col-md-3 col-sm-6">
+               <!-- col-md-3 col-sm-6 Starts -->
+               <h4>Get the news</h4>
+               <p class="text-muted">
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+               </p>
+               <form action="https://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=computerfever', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true">
+                  <!-- form Starts -->
+                  <div class="input-group">
+                     <!-- input-group Starts -->
+                     <input type="text" class="form-control" name="email">
+                     <input type="hidden" value="computerfever" name="uri"/>
+                     <input type="hidden" name="loc" value="en_US"/>
+                     <span class="input-group-btn">
+                        <!-- input-group-btn Starts -->
+                        <input type="submit" value="subscribe" class="btn btn-default">
+                     </span>
+                     <!-- input-group-btn Ends -->
+                  </div>
+                  <!-- input-group Ends -->
+               </form>
+               <!-- form Ends -->
+               <hr>
+               <h4> Stay in touch </h4>
+               <p class="social">
+                  <!-- social Starts --->
+                  <a href="#"><i class="fa fa-facebook"></i></a>
+                  <a href="#"><i class="fa fa-twitter"></i></a>
+                  <a href="#"><i class="fa fa-instagram"></i></a>
+                  <a href="#"><i class="fa fa-google-plus"></i></a>
+                  <a href="#"><i class="fa fa-envelope"></i></a>
+               </p>
+               <!-- social Ends --->
+            </div>
+            <!-- col-md-3 col-sm-6 Ends -->
+         </div>
+         <!-- row Ends -->
+      </div>
+      <!-- container Ends -->
+   </div>
+   <!-- footer ends -->
+   <!--======= COPY RIGHT ======= -->
+   <div id="copyright">
+      <!-- copyright Starts -->
+      <div class="container" >
+         <!-- container Starts -->
+         <div class="col-md-6" >
+            <!-- col-md-6 Starts -->
+            <p class="pull-left"> &copy; 2016 Mohammed Tahir Ahmed </p>
+         </div>
+         <!-- col-md-6 Ends -->
+         <div class="col-md-6" >
+            <!-- col-md-6 Starts -->
+            <p class="pull-right" >
+               Template by <a href="http://www.computerfever.com" >Computerfever.com</a>
+            </p>
+         </div>
+         <!-- col-md-6 Ends -->
+      </div>
+      <!-- container Ends -->
+   </div>
+   <!-- copyright Ends -->
+   <!--======= SCRIPTS ======= -->
    <script src="js/jquery.min.js"> </script>
    <script src="js/bootstrap.min.js"></script>
 </body>
